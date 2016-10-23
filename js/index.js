e3ae864e6fa042258979ced50c3eb8e2448cf6c4
lucl310 <lucthehuman21@gmail.com>
2016-10-23 09:19:22 -1000

    added some basic html and js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var prizes = ["5 extra minutes of break", "5 extra minutes of break", "5 extra minutes of break", "Everyone on this team gets a sticker", "Everyone on this team gets a sticker", "10 extra minutes of break", "one person on this team gets a shirt", "one person on this team gets a shirt", "spin again", "spin again", "nothing", "teachers have to sing a song of your choosing", "teachers have to sing a song of your choosing"]
+var one = {
+	break: 0,
+	break2: 0,
+	sticker: 0,
+	shirt: 0,
+	yukiosingz: 1
+};
+var whatyouget = function (break, break2, sticker, shirt, yukiosingz) {
+	document.getElementById('result') math.random() math.floor(0) math.roof(11)
+}
